ef7fcd40f31610716c76e761d2e9aaf5e3019831
Archangel <michaeldavidbaskoro@ymail.com>
2021-01-09 01:39:55 +0700

    change kernel back to 5, change niter to 500

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ parser.add_argument('--texture_scale', type=float, default=1.0,help='scale textu
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=0)#0 means a single main process
 parser.add_argument('--output_folder', default='.', help='folder to output images and model checkpoints')
 ##neural network parameters
-parser.add_argument('--kernel_size', type=int, default=7, help='size of kernel, default 5')
+parser.add_argument('--kernel_size', type=int, default=5, help='size of kernel, default 5')
 parser.add_argument('--batch_size', type=int, default=16, help='input batch size')
 parser.add_argument('--image_size', type=int, default=160, help='the height / width of the input image to network')
 parser.add_argument('--shuffle_ds', type=bool, default=False, help='shuffle dataset, default is True')
@@ -24,7 +24,7 @@ parser.add_argument('--BN_D', type=bool, default=True,help='Batch Norm of Discri
 parser.add_argument('--nBlocks', type=int, default=0,help='additional res blocks for complexity in the unet')
 parser.add_argument('--Ubottleneck', type=int, default=-1,help='Unet bottleneck, leave negative for default wide bottleneck')
 ##Optimisation parameters
-parser.add_argument('--niter', type=int, default=100, help='number of epochs to train for')
+parser.add_argument('--niter', type=int, default=500, help='number of epochs to train for')
 parser.add_argument('--lrG', type=float, default=0.0002, help='learning rate for Generator, default=0.0002')
 parser.add_argument('--lrD', type=float, default=0.0002, help='learning rate for Discriminator, default=0.0002')
 parser.add_argument('--beta1', type=float, default=0.5, help='beta1 for adam. default=0.5')
