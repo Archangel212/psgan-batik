99edc9709a8a7fd9ce644230942317956f19bf21
Michael <michaeldavidbaskoro@gmail.com>
2021-11-11 01:00:46 +0700

    add 64_Batik_500by500_homogenous_736-900 and 128_Batik_500by500_mix_736-900, update paperspace_train.sh to train 64_Batik_500by500_homogenous_736-900 and 128_Batik_500by500_mix_736-900 sequentially

--- paperspace_train.sh
+++ paperspace_train.sh
@@ -1,29 +1,35 @@
-batik_dataset_name=32_Batik_500by500_homogenous_736-900
+training()
+{
+    batik_dataset_name=$1
 
-#separate model hyperparameters and model state into two recursive directories
-model_hyperparameters=kernel=5,zl_dim=40,zg_dim=20,zp_dim=3,ngf=128,ndf=128,batch_size=16
-model_state=MLP,samefakeimg,G_upsampleConv2d,instance_noise_mean=0.1,shuffle_ds=False,nBlocks=2,real_label_smoothing=1
-experiment_name="$model_hyperparameters/$model_state"  
+    #separate model hyperparameters and model state into two recursive directories
+    model_hyperparameters=kernel=5,zl_dim=40,zg_dim=20,zp_dim=3,ngf=128,ndf=128,batch_size=16
+    model_state=MLP,samefakeimg,G_upsampleConv2d,instance_noise_mean=0.1,shuffle_ds=False,nBlocks=2,real_label_smoothing=1
+    experiment_name="$model_hyperparameters/$model_state"  
 
-#make directory for batik dataset if it didn't exist in log directory 
-mkdir -p ./log/$batik_dataset_name/$experiment_name
+    #make directory for batik dataset if it didn't exist in log directory 
+    mkdir -p ./log/$batik_dataset_name/$experiment_name
 
-python PSGAN.py --texture_path batik_dataset/$batik_dataset_name --output_folder ./log/$batik_dataset_name/$experiment_name >> ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
+    python PSGAN.py --texture_path batik_dataset/$batik_dataset_name --output_folder ./log/$batik_dataset_name/$experiment_name >> ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
 
 
-#tail the log to get time lapse
-cat ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt | tail -20 > "./log/$batik_dataset_name/$experiment_name/$model_state.txt"
-rm ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
+    #tail the log to get time lapse
+    cat ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt | tail -20 > "./log/$batik_dataset_name/$experiment_name/$model_state.txt"
+    rm ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
 
-#change log from txt to jpg format
-soffice --convert-to jpg ./log/$batik_dataset_name/$experiment_name/$model_state.txt --outdir ./log/$batik_dataset_name/$experiment_name/
+    #change log from txt to jpg format
+    soffice --convert-to jpg ./log/$batik_dataset_name/$experiment_name/$model_state.txt --outdir ./log/$batik_dataset_name/$experiment_name/
 
-msg="Training ${batik_dataset_name} with ${experiment_name}"
+    msg="Training ${batik_dataset_name} with ${experiment_name}"
 
-git add --all
-git commit -m "$msg"
-git push -f
+    git add --all
+    git commit -m "$msg"
+    git push -f
 
-echo "Finished" $msg
+    echo "Finished" $msg
+}
+
+training 64_Batik_500by500_homogenous_736
+training 128_Batik_500by500_mix_736-900
 
 sudo poweroff
