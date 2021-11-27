a4c7ccd7d1f7ee125f87386b7262a21d94ee01a3
Michael <michaeldavidbaskoro@gmail.com>
2021-11-27 13:02:02 +0700

    update paperspace_train.sh to do more than 1 training, set nBlocks to 1 and 2

--- paperspace_train.sh
+++ paperspace_train.sh
@@ -3,14 +3,14 @@ training()
     batik_dataset_name=$1
 
     #separate model hyperparameters and model state into two recursive directories
-    model_hyperparameters=kernel=5,zl_dim=20,zg_dim=40,zp_dim=4,ngf=64,ndf=64,batch_size=25
-    model_state=MLP,samefakeimg,G_upsampleConv2d,instance_noise_mean=0.1,shuffle_ds=False,real_label_smoothing=1
+    model_hyperparameters=$2
+    model_state=$3
     experiment_name="$model_hyperparameters/$model_state"  
 
     #make directory for batik dataset if it didn't exist in log directory 
     mkdir -p ./log/$batik_dataset_name/$experiment_name
 
-    python PSGAN.py --texture_path batik_dataset/$batik_dataset_name --output_folder ./log/$batik_dataset_name/$experiment_name >> ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
+    python PSGAN.py --texture_path batik_dataset/$batik_dataset_name --output_folder ./log/$batik_dataset_name/$experiment_name --nBlocks $4 >> ./log/$batik_dataset_name/$model_hyperparameters/$model_state.txt
 
 
     #tail the log to get time lapse
@@ -29,6 +29,16 @@ training()
     echo "Finished" $msg
 }
 
-training 32_Kawung
+#experiment 1
+model_hyperparameters_1=kernel=5,zl_dim=20,zg_dim=40,zp_dim=4,ngf=64,ndf=64,batch_size=25
+model_state_1=MLP,samefakeimg,G_upsampleConv2d,instance_noise_mean=0.1,shuffle_ds=False,real_label_smoothing=1,nBlocks=1
+
+#experiment 1
+model_hyperparameters_2=kernel=5,zl_dim=20,zg_dim=40,zp_dim=4,ngf=64,ndf=64,batch_size=25
+model_state_2=MLP,samefakeimg,G_upsampleConv2d,instance_noise_mean=0.1,shuffle_ds=False,real_label_smoothing=1,nBlocks=2
+
+
+training 32_Kawung $model_hyperparameters_1 $model_state_1 1
+training 32_Kawung $model_hyperparameters_2 $model_state_2 2
 
 sudo poweroff
